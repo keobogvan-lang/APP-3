6fe24292fe796acdf7c8262f0f50def9bb4f6d13
keobogvan-lang <keo.bogvan@gmail.com>
2026-05-20 17:48:18 +0200

    Update Code for APP.py
    mm

--- Code for APP.py
+++ Code for APP.py
@@ -1,7 +1,7 @@
 import csv
-
-with open ('parcoursup_massive_500000', 'parcoursup_medium_100000','parcoursup_programs_massive_5000','parcoursup_programs_medium_2500', 'parcoursup_programs_small_800','parcoursup_small_10000' ,'r') as file:
-  reader = csv.reader(file, delimiter=';')
-  for row in reader:
-    print (row)
-
+def open_file(filename):
+  with open ('parcoursup_massive_500000', 'parcoursup_medium_100000','parcoursup_programs_massive_5000','parcoursup_programs_medium_2500', 'parcoursup_programs_small_800','parcoursup_small_10000' ,'r') as file:
+    reader = csv.reader(file, delimiter=';')
+    for row in reader:
+      print (row)
+  
